38a2b6f4a2050d89b557582beaae432c2465a062
sean-bailey <seanbailey518@gmail.com>
2023-07-11 16:27:39 -0400

    Demonstration on how to build a dataset for aidetector

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # aidetector-example
-This will provide an example on how to build and train a dataset using aidetector
+
+This project generates a dataset for aidetector based on the works found in Project Gutenberg and an LLM of your choice.
+
+## Installation
+
+1. Clone this repository.
+2. Install the required Python packages:
+
+    ```
+    pip3 install -r requirements.txt
+    ```
+
+## Usage
+
+You can run the script with the following command:
+
+```
+python3 build_dataset.py --filename output.csv --books 10 --aigen --model /path/to/your/model
+```
+
+Here's what each argument does:
+
+- `--filename`: The name of the file to write. This argument is required.
+- `--books`: The number of Gutenberg books to randomly sample from. The default is 10.
+- `--aigen`: If this flag is thrown, the script will generate AI output to store in the dataset. You must also specify a model using `--model`.
+- `--model`: Specify a model to use with `--aigen`. This argument is required when `--aigen` is used.
+- `--quotes`: If this flag is thrown, the script will pull quotes from the internet to provide something to read while AI is generating output.
+- `--readbook`: If this flag is thrown, the script will select one of the books at random and provide its story, sentence by sentence, providing something to read while the AI is generating text. If both `--quotes` and `--readbook` are thrown, `--quotes` will be ignored.
+
+The script will generate a CSV file with the specified filename. The CSV file will have two columns: "classification" and "text". The "classification" column will contain 1 for AI-generated text and 0 for human-generated text. The "text" column will contain the text.
+
+Given the nature of AI model architectures, generating AI output for this dataset can be a time-consuming process. However, it's important to note that this script is designed for illustrative purposes and it generates a new, randomized dataset with each run. This provides the flexibility to create diverse datasets sourced from the public domain works available on Project Gutenberg. Moreover, you have the freedom to choose a model compatible with llama.cpp to generate the AI output, allowing you to tailor the process to your specific needs and preferences.
---
+++ build_dataset.py
@@ -0,0 +1,174 @@
+"""
+In here we just want to build the dataset for human input.
+
+"""
+
+from gutenbergpy.gutenbergcache import GutenbergCache
+import random
+import gutenbergpy.textget
+import nltk
+import csv
+from halo import Halo
+import argparse
+nltk.download('punkt')
+from llama_cpp import Llama
+from tqdm import tqdm
+from quoters import Quote
+
+
+colors = ["\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m"]
+RESET = "\033[0m"
+
+
+spinner_styles = [
+    'dots', 'dots2', 'dots3', 'dots4', 'dots5', 'dots6', 'dots7', 'dots8', 'dots9',
+    'dots10', 'dots11', 'dots12', 'line', 'line2', 'pipe', 'simpleDots',
+    'simpleDotsScrolling', 'star', 'star2', 'flip', 'hamburger', 'growVertical',
+    'growHorizontal', 'balloon', 'balloon2', 'noise', 'bounce', 'boxBounce',
+    'boxBounce2', 'triangle', 'arc', 'circle', 'squareCorners', 'circleQuarters',
+    'circleHalves', 'squish', 'toggle', 'toggle2', 'toggle3', 'toggle4', 'toggle5',
+    'toggle6', 'toggle7', 'toggle8', 'toggle9', 'toggle10', 'toggle11', 'toggle12',
+    'toggle13'
+]
+
+potential_delimiters = [
+    ',', ';', '|', '\t', ':', ' ', 
+    '#', '~', '^', '&', '*', '%', 
+    '$', '@', '!', '?', '+', '-', 
+    '=', '<', '>', '/', '\\', '`', 
+    '[', ']', '{', '}', '(', ')', 
+    '.', '_', '§', '°', '£', '€'
+]
+
+
+def find_delimiter(data):
+    
+    # Convert data to a single string
+    data_str = str(data)
+
+    # Find a character that's not in the data
+    for delimiter in potential_delimiters:
+        if delimiter not in data_str:
+            return delimiter
+
+    # If all potential delimiters are in the data, raise an error
+    raise ValueError("Couldn't find a suitable delimiter")
+
+
+
+def createCache():
+    GutenbergCache.create(refresh=True, download=True, unpack=True, parse=True, cache=True, deleteTemp=True)
+    cache  = GutenbergCache.get_cache()
+    return cache
+
+def getRandomBookText(cache):
+    rawtext=""
+    while len(rawtext)<1:
+        raw_book=gutenbergpy.textget.get_text_by_id(random.choice(cache.query(downloadtype=['application/plain','text/plain','text/html; charset=utf-8'], language=['en'])))
+        rawtext=raw_book.decode()
+        rawtext=raw_book.decode()
+        clean_book = gutenbergpy.textget.strip_headers(raw_book)
+        text=clean_book.decode()
+    return rawtext,text
+
+def getSentences(text):
+    sentences = nltk.sent_tokenize(text)
+    return sentences
+
+def group_sentences(sentences, group_size=5):
+    groups = []
+    for i in range(0, len(sentences), group_size):
+        group = " ".join(sentences[i:i+group_size])
+        groups.append(group)
+    return groups
+
+def generateAIOutput(inputtext,llm,tokens=128):
+    mysent=random.choice(inputtext)
+    print(mysent)
+    output = llm("Q: Determine the style of this passage and write a different passage in a similar style. Do not describe the style or the passage, provide your passage only: "+mysent+" A: ", max_tokens=tokens, stop=["Q:"], echo=False)
+    return output['choices'][0]['text']
+
+def write_to_csv(data, filename):
+    delimiter = find_delimiter(data)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile,delimiter=delimiter)
+        writer.writerow(["Classification", "Text"])  # write header
+        for item in data:
+            for key, value in item.items():
+                writer.writerow([key, value])  # write row
+#make sure the dataset puts 1 next to AI Generated text, and 0 next to human generated text.
+def generateFinalList(humansent,aisent):
+    returnlist=[]
+    for item in humansent:
+        if len(item)>1:
+            returnlist.append({0:item})
+    for thing in aisent:
+        if len(thing)>1:
+            returnlist.append({1:thing})
+    random.shuffle(returnlist)
+    return returnlist
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate a dataset for aidetector')
+    parser.add_argument('--filename', type=str, help='The name of the file to write', required=True)
+    parser.add_argument('--aigen', dest='aigen', action='store_true', help='Generate AI output to store in the dataset. If this flag is thrown, you must also specify a model using --model',required=False)
+    parser.add_argument('--model', type=str, help='Specify a model to use with --aigen',required=False)
+    parser.add_argument('--books', type=int, default=10, help='The number of gutenberg books to randomly sample from (default 10)',required=False)
+    parser.add_argument('--quotes', dest='quotes', action='store_true', help='Pull quotes from the internet to provide something to read while AI is generating output.',required=False)
+    parser.add_argument('--readbook', dest='readbook', action='store_true', help='Select one of the books at random and provide its story, sentence by sentence, providing something to read while the AI is generating text.',required=False)
+
+
+    parser.set_defaults(aigen=False)
+    parser.set_defaults(quotes=False)
+    parser.set_defaults(readbook=False)
+
+
+    args = parser.parse_args()
+
+    if args.aigen and args.model is None:
+        parser.error("--model is required when --aigen is used")
+    if args.readbook and args.quotes:
+        args.quotes=False
+
+    with Halo(text="Generating dataset...", spinner=random.choice(spinner_styles)) as spinner:
+        #We need to get the groups
+        finalsentgroup=[]
+        booklist=[]
+        cache=createCache()
+        for i in range(args.books):
+            spinner.text="Processing book "+str(i)
+            spinner.spinner=random.choice(spinner_styles)
+            rawtext,text=getRandomBookText(cache)
+            currentsentences=getSentences(text)
+            if args.readbook:
+                for sent in currentsentences:
+                    booklist.append(sent)
+            sentgroup=group_sentences(currentsentences)
+            for group in sentgroup:
+                finalsentgroup.append(group)
+        #now, we need to create AI output based on this final sentence group.
+        aisentgroup=[]
+        booklist.reverse()
+        if args.aigen:
+            llm = Llama(model_path=args.model,verbose=False)
+            pbar = tqdm(finalsentgroup, desc="Generating AI Output")
+            for i, sent in enumerate(pbar):
+                mq="Generating AI Output..."
+                if args.quotes:
+                    mq=Quote.print()
+                elif args.readbook:
+                    mq=booklist.pop()
+                
+                color = colors[i % len(colors)]
+                pbar.set_description(f"{color}{mq}{RESET}")
+                spinner.text="Generating AI portion of dataset... "
+                spinner.spinner = random.choice(spinner_styles)
+                aisentgroup.append(generateAIOutput(sent,llm))
+        spinner.text="Generating final list..."
+        spinner.spinner=random.choice(spinner_styles)
+        finallist=generateFinalList(finalsentgroup,aisentgroup) 
+        write_to_csv(finallist, args.filename)
+
+
+if __name__ == "__main__":
+    main()
